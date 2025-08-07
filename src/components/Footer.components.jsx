847004d253a5620949814e3a3c8642640fea84fa
Jacob Jordan <jacobjordan94@live.com>
2025-08-06 22:55:17 -0500

    improvements to controls / navigation

--- src/components/Footer.components.jsx
+++ src/components/Footer.components.jsx
@@ -1,52 +1,87 @@
 import { useContext } from "react";
 import { useNavigate } from "react-router";
 import { GlobalContext } from "../main";
+import { HeartIcon, HomeIcon, QueueListIcon } from "@heroicons/react/24/solid";
+import { HeartIcon as HeartOutlineIcon, HomeIcon as HomeOutlineIcon, QueueListIcon as QueueListOutlineIcon } from "@heroicons/react/24/outline";
 
 export default function Footer({ currentPage, setCurrentPage }) {
 
-  const navigate = useNavigate();
-  function onNavigate(page) {
-    navigate('/' + page);
-    setCurrentPage(page);
-  }
-  const NavigationButton = ({ to, name }) => 
-    <button 
-        className='w-full h-full rounded-none' 
-        onClick={() => onNavigate(to)}
-    >{ name }</button>
+    const { globalState: { currentSong } } = useContext(GlobalContext);
+  
+    const navigate = useNavigate();
+    function onNavigate(page) {
+        navigate('/' + page);
+        setCurrentPage(page);
+    }
 
-  return (
-    <footer className=''>
-      <div className="footer-wrap my-0">
-        { !(currentPage === '') && <NowPlaying /> }
-        <div className="navigation-buttons flex">
-          <div className="button-wrap flex-1/3">
-            <NavigationButton name='Likes' to='likes' />
-          </div>
-          <div className="button-wrap flex-1/3">
-            <NavigationButton name='Home' to='' />
-          </div>
-          <div className="button-wrap flex-1/3">
-            <NavigationButton name='Recents' to='recents' />
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+    return ( currentSong &&
+        <footer className=''>
+            <div className="footer-wrap">
+                <div style={{'--tw-shadow-color': 'black'}} className="relative rounded-t-4xl overflow-hidden dark-alpha-7 shadow-md">
+                    <div className="z-[-1] absolute top-0 left-0 right-0 bottom-0 filter-[blur(5px)] pointer-events-none">
+                    { (currentSong.thumbnail_uri !== '') && 
+                        <img className="size-full object-cover" src={currentSong.thumbnail_uri} />
+                    }
+                    </div>
+                    <div className="controls">
+                        { !(currentPage === '') && <NowPlaying currentSong={currentSong} /> }
+                        <Navigation currentPage={currentPage} onNavigate={onNavigate} />
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 }
 
-function NowPlaying() {
-  const { globalState: { currentSong } } = useContext(GlobalContext);
-  return ( currentSong && 
-    <div className='now-playing flex px-4 py-1.5'>
-      <div className="album-art rounded-lg overflow-hidden">
-        { currentSong.thumbnail_uri && <img className='size-16' src={ currentSong.thumbnail_uri } alt="" /> }
-      </div>
-      <div className="info flex flex-col ps-4 text-wrap">
-        <div className='artist-name flex-auto text-lg'>{ currentSong.artist }</div>
-        <div className='track-name flex-auto'>{ currentSong.song }</div>
-      </div>
-    </div>
-  );
+function Navigation({ currentPage, onNavigate }) {
+    const NavigationButton = ({ to, name, children }) => {
+        return <button 
+            className='w-full h-full rounded-none' 
+            onClick={() => onNavigate(to)}
+        >
+            <div className="button-wrap flex flex-col items-center">
+                <div className="icon-wrap">{ children }</div>
+                <div className="name font-semi-bold">{ name }</div>
+            </div>
+        </button>;
+    }
 
+    return(
+        <div className="navigation-buttons flex">
+          <div className="button-wrap flex-1/3">
+            <NavigationButton name='Likes' to='likes'>
+                { currentPage === 'likes' ? <HeartIcon className="size-8" /> : <HeartOutlineIcon className="size-8" /> }
+            </NavigationButton>
+          </div>
+          <div className="button-wrap flex-1/3">
+            <NavigationButton name='Home' to='' >
+                { currentPage === '' ? <HomeIcon className="size-8" /> : <HomeOutlineIcon className="size-8" /> }
+            </NavigationButton>
+          </div>
+          <div className="button-wrap flex-1/3">
+            <NavigationButton name='Recents' to='recents'>
+                { currentPage === 'recents' ? <QueueListIcon className="size-8" /> : <QueueListOutlineIcon className="size-8" /> }
+            </NavigationButton>
+          </div>
+        </div>
+    );
+}
+
+function NowPlaying({ currentSong }) {
+
+    return ( currentSong && 
+    <div className="now-playing flex p-4 pb-0">
+        <div className="album-art rounded-lg overflow-hidden">
+        { currentSong.thumbnail_uri && <img className='size-16' src={ currentSong.thumbnail_uri } alt="" /> }
+        </div>
+        <div className="info flex flex-col ps-4">
+            <div className='artist-name text-lg'>
+                { currentSong.artist }
+            </div>
+            <div className='track-name flex-auto'>
+                { currentSong.song }
+            </div>
+        </div>
+    </div>
+    );
 }
