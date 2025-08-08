646ba23bb00b587f401216840c90098a29f2a264
Jacob Jordan <jacobjordan94@live.com>
2025-08-07 20:11:27 -0500

    move albumart to seperate file

--- src/pages/Recents.jsx
+++ src/pages/Recents.jsx
@@ -1,12 +1,10 @@
 import { useEffect } from "react";
 import useRecents from "../hooks/Recents";
+import AlbumArt from "../components/AlbumArt";
 
 function Recents() {
 
     const [ recents ] = useRecents();
-    useEffect(() => {
-        console.log('recents', recents);
-    }, [recents])
 
     return ( recents && 
         <div className="page page-recents flex flex-col gap-2 p-2">
@@ -21,7 +19,9 @@ const RecentCard = ({ song }) => {
     return ( song && 
         <div className="recent-card flex rounded-xl p-2 backdrop-blur-md">
             <div className="album-art-wrapper rounded-md overflow-hidden pe-2">
-                <AlbumArt image={song.image_uri} />
+                <div className="size-16">
+                    <AlbumArt image={song.image_uri} />
+                </div>
             </div>
             <div className="info">
                 <div className="artist-name text-lg">{ song.artist }</div>
@@ -30,15 +30,5 @@ const RecentCard = ({ song }) => {
         </div>
     );
 };
-
-function AlbumArt({ image, size = 16 }) {
-    return <div className={"album-art inline-flex items-center justify-center bg-neutral-800 size-" + size}>
-    { 
-        image === '' ? 
-            <span className="text-neutral-700 text-4xl">?</span> : 
-            <img className="w-full h-full object-cover" src={image} />
-    }
-    </div>
-}
 
 export default Recents;
