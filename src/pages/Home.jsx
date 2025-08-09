d250907ad9c0849c6f5b290cb285702c2651a443
Jacob Jordan <jacobjordan94@live.com>
2025-08-08 22:57:28 -0500

    one more border

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     const { globalState: { currentSong } } = useContext(GlobalContext);
 
     return (
-        <div className="page page-home w-full h-full flex flex-col">
+        <div className="page page-home w-full h-full flex flex-col border-4 transparent-border-dark">
             <div className="home-information-wrapper flex-grow flex items-center justify-center">
                 <div className="w-10/12 inline-flex flex-col gap-6">
                     <HomeInformation currentSong={currentSong} />
