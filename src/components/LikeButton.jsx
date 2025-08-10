132201438ffff29e39d1dde37929abf7ef9ff962
Jacob Jordan <jacobjordan94@live.com>
2025-08-10 18:50:54 -0500

    add like button

---
+++ src/components/LikeButton.jsx
@@ -0,0 +1,32 @@
+import { HeartIcon } from "@heroicons/react/24/solid";
+import { HeartOutlineIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react"
+
+function LikeButton({ className, currentSong }) {
+    const { globalState: { likedSongs: { songs, dispatch } } } = useContext(GlobalContext);
+    const [ liked, setLiked ] = useState();
+
+    function isLiked() {
+        return songs.findIndex(song => song.id === currentSong.id) > -1;
+    }
+
+    function toggleLike() {
+        if(liked) {
+            dispatch({ type: 'remove', id: currentSong.id })
+        } else {
+            dispatch({ type: 'add', song: {...currentSong} });
+        }
+    }
+
+    useEffect(() => {
+        if(!currentSong || !songs) return;
+        setLiked(isLiked());
+    }, [ currentSong, songs ]);
+
+    return liked && 
+        <button disabled={currentSong.play_type !== 'trackplay'} className={'reset-padding song-like-button ' + className} onClick={toggleLike}>
+            { liked ? <HeartIcon /> : <HeartOutlineIcon /> }
+        </button>;
+}
+
+export default LikeButton;
