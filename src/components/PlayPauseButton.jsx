b020c30d771a3ab9e9e7f11efb372bdda2fc2d38
Jacob Jordan <jacobjordan94@live.com>
2025-08-10 19:01:19 -0500

    add play pause button

---
+++ src/components/PlayPauseButton.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import { PlayIcon, PauseIcon } from "@heroicons/react/24/solid";
+
+function PlayPauseButton({ className }) {
+
+    const [ isPlaying, setIsPlaying ] = useState(false);
+    function togglePlayPause() {
+        setIsPlaying(!isPlaying);
+    }
+
+    return <button className={'reset-padding play-pause-button ' + className} onClick={togglePlayPause}>
+        isPlaying ? <PauseIcon /> : <PlayIcon />
+    </button>
+}
+
+export default PlayPauseButton;
