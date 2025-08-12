1e08be3b6b66cb23cafd14b35f44fbfa22a7b710
Jacob Jordan <jacobjordan94@live.com>
2025-08-11 21:11:24 -0500

    create host-image component

--- src/components/CurrentShow.components.tsx
+++ src/components/CurrentShow.components.tsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import AlbumArt from "./AlbumArt";
+import AlbumArt, { Image } from "./AlbumArt";
 import PictureWithInfo from "./PictureWithInfo";
 import { GlobalContext } from "../main";
 
@@ -13,11 +13,17 @@ function CurrentShowMini({}) {
             title={currentShow.program_name}
             subtitle={ currentShow.host_names.join(', ') }
         >
-            <div className={"show-host-image size-12 overflow-hidden rounded-full"}>
-                <AlbumArt className="show-host-image size-12 overflow-hidden rounded-full" image={ currentShow.image_uri } />
-            </div>
+            <HostImage image={ currentShow.image_uri }  />
         </PictureWithInfo>
     )
+}
+
+function HostImage({ size = 'size-12' , image }) {
+    return  <Image image={image} className={"show-host-wrapper overflow-hidden rounded-full " + size}>
+                <div className="backdrop-blur-md p-1 w-full h-full">
+                    <Image image={image} className="show-host-image w-full h-full overflow-hidden rounded-full" />
+                </div>
+            </Image>
 }
 
 export default CurrentShowMini;
