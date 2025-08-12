c9a4d282ba32ab280d507bf7b0848e088ea400eb
Jacob Jordan <jacobjordan94@live.com>
2025-08-11 19:12:30 -0500

    better responsive design

--- src/pages/Home.components.jsx
+++ src/pages/Home.components.jsx
@@ -17,12 +17,12 @@ import { GlobalContext } from "../main";
 export function HomeInformation({ currentSong }) {
 
     return ( currentSong &&
-        <div className="home-information-wrapper w-full rounded-2xl backdrop-blur-2xl p-4 flex gap-4 border-4 transparent-border-dark sm:w-full sm:h-full lg:flex-col lg:justify-between">
-            <div className="sm:w-full sm:flex sm:flex-col lg:flex-row">
-                <div className="album-art-wrapper rounded-2xl border-4 transparent-border-light overflow-hidden sm:flex-grow">
-                    <AlbumArt className="w-full h-full object-cover lg:w-auto lg:h-auto" image={currentSong.image_uri}></AlbumArt>
+        <div className="home-information-wrapper w-full rounded-2xl backdrop-blur-2xl p-4 flex gap-4 border-4 transparent-border-dark sm:w-full h-full lg:flex-col lg:justify-between">
+            <div className="w-full flex flex-col flex-grow lg:flex-row lg:flex-grow">
+                <div style={{ backgroundImage: `url(${currentSong.image_uri})` }} className="album-art-wrapper rounded-2xl border-4 transparent-border-light overflow-hidden flex-grow h-full">
+                    {/* <AlbumArt className="w-full h-full object-cover lg:w-auto lg:h-auto" image={currentSong.image_uri}></AlbumArt> */}
                 </div>
-                <div className="flex flex-col flex-grow justify-between sm:flex-row pt-4 lg:grow-0">
+                <div className="flex flex-col flex-grow justify-between sm:flex-row sm:grow-0 pt-4 lg:grow-0">
                     <SongInformation currentSong={currentSong} />
                     <HomeButtons offset={false} className="hidden gap-6 sm:flex items-center lg:hidden" currentSong={currentSong}></HomeButtons>
                 </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
     const { globalState: { currentSong } } = useContext(GlobalContext);
 
     return (
-        <div className="page page-home w-full h-full flex flex-col border-4 transparent-border-dark p-6 md:p-12">
+        <div className="page page-home w-full h-full flex flex-col border-4 transparent-border-dark p-6 gap-4">
             <HomeInformation currentSong={currentSong} />
             <div className="controls-wrapper flex flex-grow items-end sm:hidden">
                 <HomeButtons className="flex flex-row w-full justify-evenly" currentSong={currentSong} />
