cb8b2da088392a9bcf2dc174e3539ae314732314
Jacob Jordan <jacobjordan94@live.com>
2025-08-14 15:35:19 -0500

    write some hooks that probably wont be used

---
+++ src/hooks/ITunes.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import useFetch from "./Fetch";
+
+function useITunes(artist, song) {
+    const [state, setState] = useState();
+    const [ artistResponse ] = useFetch('https://itunes.apple.com/search', {
+        term: artist,
+        media: 'music',
+        entity: 'musicArtist',
+        limit: 1,
+    });
+    const [ songResponse ] = useFetch('https://itunes.apple.com/search', {
+        term: `${artist}+${song}`,
+        media: 'music',
+        entity: 'song',
+        limit: 1,
+    })
+    
+    useEffect(() => {
+        if(artistResponse && songResponse) {
+            console.log('Artist response');
+            console.log(artistResponse);
+            console.log('Song response');
+            console.log(songResponse);
+            setState({ artistResponse, songResponse });
+        }
+    }, [ artistResponse, songResponse ])
+
+    return [ state ];
+}
+
+export default useITunes;
---
+++ src/hooks/WikiImage.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import useFetch from "./Fetch";
+
+function useWikiImage(artistName) {
+    const formattedName = artistName.replace(/\s+/g, '_');
+    const [ response ] = useFetch('https://en.wikipedia.org/w/api.php', {
+        action: 'query',
+        format: 'json',
+        titles: formattedName,
+        prop: 'pageImages',
+        piprop: 'original',
+    });
+    const [ state, _setState ] = useState();
+
+    function setState() {
+        const pages = response.query.pages;
+        const pageId = Object.keys(pages)[0];
+        if(pages[pageId].hasOwnProperty('original')) {
+            const imageUrl = pages[pageId].original.source;
+            setState(imageUrl);
+        } else {
+            setState(null)
+        }
+    }
+
+    useEffect(() => {
+        if(!response) return;
+        setState();
+    }, [response]);
+
+    return [ state ];
+}
+
+export default useWikiImage;
