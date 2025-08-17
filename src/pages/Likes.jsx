4112ca7cce09b90f326efe5d0c26c0c0feff1045
Jacob Jordan <jacobjordan94@live.com>
2025-08-16 22:59:55 -0500

    update likes and recents page to use song list components

--- src/pages/Likes.jsx
+++ src/pages/Likes.jsx
@@ -1,33 +1,12 @@
 import { useContext } from "react";
 import { GlobalContext } from "../main";
-import PictureWithInfo from "../components/PictureWithInfo";
-import { useNavigate } from "react-router";
+import { SongList } from "../components/SongList";
 
 function Likes() {
 
-    const { globalState: { likedSongs: { songs, dispatch } } } = useContext(GlobalContext);
+    const { globalState: { likedSongs: { songs } } } = useContext(GlobalContext);
 
-    return ( songs && 
-        <div className="page page-likes flex flex-col gap-3 p-2">
-        {
-            songs.map((song, i) => <LikesCard key={i} song={song} />)
-        }
-        </div>
-    );
-}
-
-const LikesCard = ({ song }) => {
-    const naviagte = useNavigate();
-    return <div 
-        className="recent-card backdrop-blur-2xl rounded-lg p-2 transparent-border-dark border-4 cursor-pointer"
-        onClick={() => naviagte('/song/' + song.id)}
-    >
-        <PictureWithInfo
-            image={song.thumbnail_uri}
-            title={song.artist}
-            subtitle={song.song}
-        ></PictureWithInfo>
-    </div>
+    return songs && <SongList songs={songs} />
 }
 
 export default Likes;
--- src/pages/Recents.jsx
+++ src/pages/Recents.jsx
@@ -1,34 +1,12 @@
 import { useContext } from "react";
 import { GlobalContext } from "../main";
-import PictureWithInfo from "../components/PictureWithInfo";
-import AlbumArt from "../components/AlbumArt";
-import { useNavigate } from "react-router";
+import { SongList } from "../components/SongList";
 
 function Recents() {
 
     const { globalState: { recents } } = useContext(GlobalContext);
 
-    return ( recents && 
-        <div className="page page-recents flex flex-col gap-3 p-2">
-        {
-            recents.map((song, i) => <RecentCard key={i} song={song} />)
-        }
-        </div>
-    );
-}
-
-const RecentCard = ({ song }) => {
-    const navigate = useNavigate();
-    
-    return <div className="recent-card backdrop-blur-2xl rounded-lg p-1 transparent-border-dark border-4 cursor-pointer"
-        onClick={() => navigate('/song/' + song.id)}
-    >
-        <PictureWithInfo
-            image={song.thumbnail_uri || song.image_uri}
-            title={song.artist}
-            subtitle={song.song}
-        ></PictureWithInfo>
-    </div>
+    return recents && <SongList songs={recents} />
 }
 
 export default Recents;
