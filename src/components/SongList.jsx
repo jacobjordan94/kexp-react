38f4f496863b3ef12f664bbd0d1a8d7a693f8d41
Jacob Jordan <jacobjordan94@live.com>
2025-08-19 17:01:30 -0500

    change song list items into cards at sm breakpoint

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -2,24 +2,28 @@ import { useNavigate } from "react-router";
 import { Image } from "./AlbumArt"
 
 export function SongList({ className, songs }) {
-    return  (<div className={"song-list flex gap-4 flex-col flex-grow " + className}> {
-                songs.map((song, i) => <SongCard key={`${song.id}-${i}`} song={song} />)
+    return  (<div className={"song-list flex gap-4 flex-col sm:flex-row sm:flex-wrap " + className}> {
+                songs.map((song, i) => 
+                    <div className="sm:flex-1/3 lg:flex-1/4 xl:flex-1/5 4xl:flex-1/7">
+                        <SongCard key={`${song.id}-${i}`} song={song} /></div>
+                    )
             }</div>);
 }
 
 export function SongCard({ song }) {
+    // Default - list-item, sm & above - card
     const navigate = useNavigate();
     return (
         <div 
             className="song-card p-2 backdrop-blur-2xl
                      rounded-lg transparent-border-dark 
-                     border-4 cursor-pointer flex gap-3"
+                     border-4 cursor-pointer flex gap-3 sm:flex-col sm:h-full"
             onClick={() => navigate('/song/' + song.id)}
         >
-            <Image image={song.thumbnail_uri} className="size-16 rounded-md overflow-hidden" />
-            <div className="information-wrap flex flex-col flex-grow justify-evenly">
+            <Image image={song.image_uri} className="size-16 rounded-md overflow-hidden sm:w-full sm:h-48" />
+            <div className="information-wrap flex flex-col">
                 <div className="artist font-semibold text-lg">{ song.artist }</div>
-                <div className="hidden album" >{ song.album  }</div>
+                <div className="hidden sm:block album" >{ song.album  }</div>
                 <div className="song text-sm"  >{ song.song   }</div>
             </div>
         </div>
--- src/layouts/SongListLayout.jsx
+++ src/layouts/SongListLayout.jsx
@@ -9,11 +9,13 @@ export default function SongListLayout() {
 
     return (
         <div className="song-list-layout flex gap-4 px-4 h-full">
-            <div className="list flex-2/3 overflow-y-scroll pt-4">
+            <div className="list flex-2/3 xl:flex-3/4 overflow-y-scroll py-4">
                 <Outlet />
             </div>
-            <div className="hidden md:block current-info flex-1/3 py-4">
-                <Show show={currentShow} />
+            <div className="hidden md:block current-info flex-1/3 xl:flex-1/4 py-4">
+                <div className="side-controls-container">
+                    <Show show={currentShow} />
+                </div>
             </div>
         </div>
     );
