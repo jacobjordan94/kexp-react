ac500179be2bd6b7dc7eb33b6f5539d2dc30e336
Jacob Jordan <jacobjordan94@live.com>
2025-08-19 21:54:06 -0500

    improvements to contol box styling

--- src/layouts/SongListLayout.jsx
+++ src/layouts/SongListLayout.jsx
@@ -17,16 +17,13 @@ export default function SongListLayout() {
                 <Outlet />
             </div>
             <div className="hidden md:flex flex-col gap-4 current-info flex-1/3 xl:flex-1/4 p-4 ps-0 h-full">
-                <div className="text-shadow-[rgba(0,0,0,0.5)\ 0px\ 4px\ 6px] text-shadow-2xl side-title text-2xl">
+                <div className="shadow-[rgba(0,0,0,0.5)\ 0px\ 4px\ 6px] shadow-xl side-title dark-alpha-1 text-2xl backdrop-blur-2xl p-4 rounded-2xl ">
                     Now Playing
                 </div>
                 <div className="side-controls-container flex flex-col gap-4 flex-grow">
                     <div className="shadow-[rgba(0,0,0,0.5)] shadow-xl overflow-hidden rounded-2xl">
                         <Show show={currentShow}/>
                     </div>
-                    {/* <div className="shadow-[rgba(0,0,0,0.5)] shadow-xl overflow-hidden rounded-2xl">
-                        <CommentBox comment={currentSong.comment} className={'flex-1'} />
-                    </div> */}
                     <div className="shadow-[rgba(0,0,0,0.5)] shadow-xl overflow-hidden rounded-2xl flex-grow">
                         <ControlsBox song={currentSong} className={'h-full'} />
                     </div>
@@ -39,20 +36,18 @@ export default function SongListLayout() {
 function ControlsBox ({ song, className }) {
     return ( song &&
         <div className={"controls-container rounded-2xl overflow-hidden " + className}>
-            <Image image={song.image_uri} className="w-full h-full">
-                <div className="controls-content backdrop-blur-2xl p-2 h-full">
-                    {/* <img src={song.image_uri} /> */}
-                    <Image image={song.image_uri} className="rounded-2xl overflow-hidden h-full">
-                        <div className="info dark-alpha-5 p-4 flex flex-col h-full justify-end">
-                            <ArtistImage artistName={song.artist} className="flex-grow rounded-2xl" />
-                            <div className="artist text-lg font-semibold mt-4">{ song.artist }</div>
-                            <div className="album font-semibold">{ song.album }</div>
-                            <div className="song font-semibold">{ song.song }</div>
-                            <div className="buttons-container mt-4">
-                                <Controls currentSong={song} className="flex flex-row justify-evenly" />
-                            </div>
-                        </div>
-                    </Image>
+            <Image image={song.image_uri} className="rounded-2xl overflow-hidden h-full">
+                <div className="info dark-alpha-2 p-4 flex flex-col h-full justify-end backdrop-blur-xs overflow-hidden">
+                    <ArtistImage artistName={song.artist} className="flex-grow rounded-2xl bg-top overflow-hidden">
+                        <div className="artist text-2xl font-semibold h-full w-full flex items-end p-4 bg-linear-to-b from-[rgba(0,0,0,0)] to-[rgba(0,0,0,1)]">{ song.artist }</div>
+                    </ArtistImage>
+                    <div className="other-info px-4 py-2">
+                        <div className="album text-lg font-semibold">{ song.album }</div>
+                        <div className="song  text-lg font-semibold">{  song.song }</div>
+                    </div>
+                    <div className="buttons-container mt-4">
+                        <Controls currentSong={song} className="flex flex-row justify-evenly" />
+                    </div>
                 </div>
             </Image>
         </div>
