eca5f3cb2c7cb3b193137fdef2d8929709c679c4
Jacob Jordan <jacobjordan94@live.com>
2025-08-20 20:39:14 -0500

    add FullArtistInfoWithLikeButton component

--- src/components/ArtistInformation.components.jsx
+++ src/components/ArtistInformation.components.jsx
@@ -1,5 +1,6 @@
 import { UserIcon, CodeBracketIcon, MusicalNoteIcon  } from "@heroicons/react/24/solid"
 import { useEffect, useState } from "react";
+import BaseLikeButton from "./BaseLikeButton";
 
 export const AlbumIcon = ({ className }) => <CodeBracketIcon className={className} />
 export const ArtistIcon = ({ className }) => <UserIcon className={className} />
@@ -8,7 +9,7 @@ export const SongIcon = ({ className }) => <MusicalNoteIcon className={className
 export function IconText({Icon, text, iconClass, textClass}) {
     return ( text && 
         <div className={"icon-text flex gap-2.5 items-center"}>
-            <div className="icon">
+            <div className={"icon " + iconClass}>
                 <Icon />
             </div>
             <div className={"text " + textClass}>{ text }</div>
@@ -28,7 +29,7 @@ export function SongInformation({ songName, iconClass, textClass }) {
     return <IconText Icon={() => <SongIcon className={iconClass}/>} text={songName} iconClass={iconClass} textClass={textClass} />
 }
 
-export function FullArtistInfo({ song, sections = ['artist', 'song', 'album'], artistIconClass, artistTextClass, songIconClass, songTextClass, albumIconClass, albumTextClass, iconClass, textClass }) {
+export function FullArtistInfo({ song, sections = ['artist', 'song', 'album'], artistIconClass, artistTextClass, songIconClass, songTextClass, albumIconClass, albumTextClass, iconClass, textClass, children, className }) {
     const [info, setInfo] = useState();
     useEffect(() => {
         if(!song) return;
@@ -40,15 +41,34 @@ export function FullArtistInfo({ song, sections = ['artist', 'song', 'album'], a
     }, [ song ]);
 
     return ( info &&
-        <div className="full-artist-info flex flex-col ">
-        {
-            sections.map((section, i) => (
-                section === 'artist' ? <ArtistInformation key={i} artistName={song.artist} iconClass={iconClass + " " + artistIconClass} textClass={textClass + " " + artistTextClass} /> :
-                section === 'song'   ? <SongInformation   key={i} songName={song.song}     iconClass={iconClass + " " + songIconClass}   textClass={textClass + " " + songTextClass}   /> : 
-                section === 'album'  ? <AlbumInformation  key={i} albumName={song.album}   iconClass={iconClass + " " + albumIconClass}  textClass={textClass + " " + albumTextClass}  /> : <></>
-            ))
-        }
+        <div className={"full-artist-info " + className}>
+            <div className="full-artist-info-inner flex items-center gap-4">
+                <div className="full-artist-info-text flex-grow flex flex-col gap-1.5">
+                {
+                    sections.map((section, i) => (
+                        section === 'artist' ? <ArtistInformation key={i} artistName={info.artist} iconClass={iconClass + " " + artistIconClass} textClass={textClass + " " + artistTextClass} /> :
+                        section === 'song'   ? <SongInformation   key={i} songName={info.song}     iconClass={iconClass + " " + songIconClass}   textClass={textClass + " " + songTextClass}   /> : 
+                        section === 'album'  ? info.album && <AlbumInformation  key={i} albumName={info.album}   iconClass={iconClass + " " + albumIconClass}  textClass={textClass + " " + albumTextClass}  /> : <></>
+                    ))
+                }
+                </div>
+                { children }
+            </div>
         </div>
+    );
+}
+
+export function FullArtistInfoWithLikeButton({ song, sections = ['artist', 'song', 'album'], artistIconClass, artistTextClass, songIconClass, songTextClass, albumIconClass, albumTextClass, iconClass, textClass, className, likeButtonSize = 'size-10' }) {
+    return ( 
+        <FullArtistInfo
+            song={song} sections={sections} artistIconClass={artistIconClass} 
+            artistTextClass={artistTextClass} songIconClass={songIconClass} 
+            songTextClass={songTextClass} albumIconClass={albumIconClass} 
+            albumTextClass={albumTextClass} iconClass={iconClass} textClass={textClass}
+            className={className}
+        >
+            <BaseLikeButton className={"me-4 " + likeButtonSize} currentSong={song} />
+        </FullArtistInfo>
     );
 }
 
