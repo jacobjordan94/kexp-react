4ad3ecd5efa5beffc1bf447e16ef45499f5c9435
Jacob Jordan <jacobjordan94@live.com>
2025-08-21 16:25:41 -0500

    add docker file

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 8320,
+    strictPort: true, 
+    host: true,
+    origin: 'https://0.0.0.0:8320',
+  }
 })
